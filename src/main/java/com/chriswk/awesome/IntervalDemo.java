376f5c0fccae4218911eb10cac9ee3d0c9412556
chrkolst <christopher.kolstad@finn.no>
2015-01-20 12:24:42 +0100

    Added interval async demo

--- src/main/java/com/chriswk/awesome/IntervalDemo.java
+++ src/main/java/com/chriswk/awesome/IntervalDemo.java
@@ -2,15 +2,24 @@ package com.chriswk.awesome;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import rx.Observable;
 
 public class IntervalDemo {
-    public static void main(String... args) {
-        Observable.interval(1, TimeUnit.SECONDS).map((x) -> x).subscribe((e) -> System.out.println("1 second delay: step [" + e + "]")
-                , System.err::println
-                , () -> System.out.println("Complete"));
-        Observable.interval(500, TimeUnit.MILLISECONDS).take(10).subscribe((e) -> System.out.println("500 ms delay: step [" + e + "]")
-                , System.err::println
-                , () -> System.out.println("Complete"));
+    private static final Logger SECOND = LogManager.getLogger("SECOND");
+    private static final Logger HALFSECOND = LogManager.getLogger("HALF");
+    public static void main(String... args) throws InterruptedException {
+        Observable.interval(1, TimeUnit.SECONDS).subscribe(
+                SECOND::debug,
+                SECOND::error,
+                () -> SECOND.info("Complete"));
+        Observable.interval(500, TimeUnit.MILLISECONDS).take(10).subscribe(
+                HALFSECOND::debug,
+                HALFSECOND::error,
+                () -> HALFSECOND.info("Complete"));
+/** Without this the method terminates
+ **/       Thread.sleep(20000);
+
     }
 }
